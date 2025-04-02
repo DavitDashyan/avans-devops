845b557f75ab6e3bdaf09ccb9475c44939c75673
DavitDashyan <davit.dashyan@icloud.com>
2025-04-02 10:20:28 +0200

    SonarCloud Try2

--- src/main/java/devops/App.java
+++ src/main/java/devops/App.java
@@ -20,7 +20,6 @@ public class App {
             System.out.println("7. Exit");
 
             System.out.print("Uw keuze: ");
-            
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline
 
